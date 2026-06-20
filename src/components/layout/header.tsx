beb76b78ebceb04a1e42e0903423f482707ed1ec
v0agent <it+v0agent@vercel.com>
2026-06-20 07:39:29 +0000

    Revert to original logo with Designs text, remove duplicate text element

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -41,7 +41,7 @@ export async function Header() {
           >
             <div className="relative flex h-20 w-20 items-center justify-center overflow-hidden md:h-24 md:w-24">
               <Image
-                src="/logo-d-clean.png"
+                src="/logo-ds-designs.png"
                 alt={`${SITE.name} logo`}
                 width={96}
                 height={96}
